1f281ed8d01bcda32fbcff04fad0d2452a342313
smalldog124 <panumars@gmail.com>
2018-07-19 15:27:40 +0700

    [Edited]: จัดหน้าใหม่

--- public/js/bingoGame.js
+++ public/js/bingoGame.js
@@ -2,11 +2,26 @@ $(function () {
     $('#startGame').click(startGame)
 })
 
+$(function () {
+    $('#random').click(random)
+})
+
 function startGame() {
     var host = "http://localhost:3000/bingogame/start"
-    $.get(host, function (responseData) {
+    var parameter = {
+        playerOne: $('#playerOne').val(),
+        playerTwo: $('#playerTwo').val()
+    }
+    $.post(url, parameter, function (responseData) {
         if (responseData == "200") {
             window.location("game.html")
         }
+    })
+}
+
+function random() {
+    var host = "http://localhost:3000/bingogame/info"
+    $.get(host, function (responseData) {
+
     })
 }
