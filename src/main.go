db4022542578b037a70f9c45a6e8c3368c192acb
teerawat.th <teerawat.th@baac.or.th>
2018-07-20 13:37:47 +0700

    [Edited]: แก้ไขไฟล์ main.go

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/bingo/play", api.PlayHandler)
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/bingogame", http.StripPrefix("/bingogame", fs))
