3c900cbd8de67558213b98ad3caec5523ef8ff53
sakarin.po <sakarin.po@baac.or.th>
2018-07-23 16:14:40 +0700

    [Edited]:แก้การเรียกใช้งาน package ให้ถูกต้อง

--- src/service/gameservice.go
+++ src/service/gameservice.go
@@ -4,31 +4,6 @@ import (
 	"bingogame"
 )
 
-type PlayGameService struct {
-	Game bingogame.Game
-}
-
-func (g *PlayGameService) PlayGame() bingogame.PlayResponse {
-	playResponse := bingogame.PlayResponse{}
-	pickupNumber := g.Game.PickUpNumber()
-
-	positionXPlayer1, positionYPlayer1 := g.Game.Players[0].CheckNumber(pickupNumber)
-	if positionXPlayer1 != -1 && positionYPlayer1 != -1 {
-		g.Game.Players[0].Mark(positionXPlayer1, positionYPlayer1)
-		if g.Game.Players[0].GetBingo(positionXPlayer1, positionYPlayer1) {
-			playResponse.Winner = g.Game.Players[0].Name
-		}
-	}
-
-	positionXPlayer2, positionYPlayer2 := g.Game.Players[1].CheckNumber(pickupNumber)
-	if positionXPlayer2 != -1 && positionYPlayer2 != -1 {
-		g.Game.Players[1].Mark(positionXPlayer2, positionYPlayer2)
-		if g.Game.Players[1].GetBingo(positionXPlayer2, positionYPlayer2) {
-			playResponse.Winner = g.Game.Players[1].Name
-		}
-	}
-	playResponse.Number = pickupNumber
-	return playResponse
 const (
 	numberOfGrid = 5
 	numberInBox  = 75
@@ -36,6 +11,11 @@ const (
 
 type GameService struct {
 	Game bingogame.Game
+}
+
+func (g *GameService) PlayGame() bingogame.PlayResponse {
+
+	return g.Game.Play()
 }
 
 type PlayerInfoResponse struct {
